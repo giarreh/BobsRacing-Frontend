9fe658d51771c0dfe2c83aafcbb17e5057e6c69e
giarreh <giarreh1@gmail.com>
2024-11-29 11:30:15 +0100

    added a clickable text to go to race

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 onClick={() => navigate('/race')}>Click here to go to racing</h1>
     </div>
   )
 }
