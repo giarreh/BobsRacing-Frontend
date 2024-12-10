274cd6cdd325c9a6435e1dacba68cf3a5a00b342
giarreh <giarreh1@gmail.com>
2024-12-10 09:05:12 +0100

    merged

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -18,18 +18,30 @@ export const UserContext = createContext({} as UserContextType);
 
 export default function UserContextProvider({ children }: UserContextProviderProps) {
   const [user, setUser] = useState<User | null>(null); // You can replace 'any' with a more specific type if you have one
+  const [loading, setLoading] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  // Get the authentication token from localStorage
+const getAuthToken = () => {
+  return localStorage.getItem('authToken');
+};
+
+const getUserFromToken = () => {
+  const decodedToken = jwtDecode(getAuthToken());
+  console.log("DECODED TOKEN: ", decodedToken);
+  return decodedToken;
+};
 
 
-  const getUserFromLocalStorage = () => {
-    const user = localStorage.getItem('user');
-    if (user) {
-      setUser(JSON.parse(user));
-    }
-  }
+// Clear the authentication token from localStorage
+const clearAuthToken = () => {
+  return localStorage.removeItem('authToken');
+};
+
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      {children}
+    <UserContext.Provider value={{ user, setUser, setAuthToken, getAuthToken, clearAuthToken,getUserFromToken }}>
+      {loading ? <p>Loading...</p> : children}
     </UserContext.Provider>
   );
 }
