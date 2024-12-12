94f52d80836479f5146edb63a86b13a89f965f88
stp <steventphung6@gmail.com>
2024-12-12 12:15:49 +0100

    modal created

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useContext } from "react";
-import { useNavigate } from "react-router";
+import { useEffect, useState, useContext } from "react";
+import { useNavigate, useParams } from "react-router";
 import { UserContext } from "../../contexts/UserContext";
 import { User } from "../../interfaces/IUser";
 
@@ -7,6 +7,7 @@ import "./ProfilePage.css";
 
 export default function ProfilePage() {
   const navigate = useNavigate();
+  //const { id } = useParams();
   const [selectedTab, setSelectedTab] = useState("general");
   const [sidebarOpen, setSidebarOpen] = useState(false); // To track sidebar open/close state
   const [backButtonVisible, setBackButtonVisible] = useState(false); // To track if the back button should be visible
@@ -17,12 +18,28 @@ export default function ProfilePage() {
   const { user, getUserFromToken } = useContext(UserContext);
 
   const [userEdit, setUser] = useState<User>({
-    id: 0,
-    profilename: "",
+    profilename: user?.profilename,
     username: user?.username,
-    credits: 0,
-    role: "User",
   });
+
+  // UseEffect
+
+  // useEffect(() => {
+  //     console.log('Fetching user');
+  //     console.log(id);
+  //     fetch(`https://localhost:7181/api/user/${id}`, {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'Authorization': `Bearer ${getAuthToken()}`,
+  //       },
+  //   })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setAthlete(data);
+  //       })
+  //       .catch((error) => console.error('Error fetching athlete:', error));
+  //   }, [id]);
 
   // Function to handle tab selection
   const handleTabSelect = (tab: string) => {
@@ -46,20 +63,17 @@ export default function ProfilePage() {
   };
 
   // Handle submissions
-
   const handleEdit = (e: React.FormEvent) => {
     e.preventDefault();
 
     // Check if all fields are valid
-    if (userEdit.profilename === "") {
-      return alert("Please fill out all fields correctly");
-    }
-
-    if (userEdit.username === "") {
-      return alert("Please fill out all fields correctly");
+    if (userEdit.profilename === "" && userEdit.username === "") {
+      return alert("Please fill out a field");
     }
 
     try {
+      console.log(JSON.stringify(userEdit));
+      console.log(user?.id)
       fetch(`https://localhost:7181/api/User/${user?.id}/credentials`, {
         method: "PUT",
         headers: {
@@ -70,17 +84,17 @@ export default function ProfilePage() {
       })
         .then((response) => {
           if (!response.ok) {
+            console.log("sadasda" + JSON.stringify(userEdit));
             throw new Error(`HTTP error! status: ${response.status}`);
           }
         })
         .then((data) => {
-          console.log("Success: Athlete updated", data);
-          alert("Athlete updated successfully");
-          navigate("/profile");
+          console.log("Success: Profile updated", data);
+          alert("Profile updated successfully");
         });
     } catch (error) {
-      console.error("Error updating athlete:", error);
-      alert("Error updating athlete");
+      console.error("Error updating profile:", error);
+      alert("Error updating profile");
     }
   };
 
@@ -142,30 +156,28 @@ export default function ProfilePage() {
               </label>
             </div>
           ) : selectedTab === "settings" ? (
-            <div className="settings-content">
-              <form onSubmit={handleEdit}>
-                <label>Name: </label>
-                <input
-                  type="text"
-                  placeholder={user?.profilename}
-                  value={userEdit.profilename}
-                  onChange={(e) =>
-                    setUser({ ...userEdit, profilename: e.target.value })
-                  }
-                />
-                <label>Username: </label>
-                <input
-                  type="text"
-                  placeholder={user?.username}
-                  value={userEdit.username}
-                  onChange={(e) =>
-                    setUser({ ...userEdit, username: e.target.value })
-                  }
-                />
+            <form className="settings-content" onSubmit={handleEdit}>
+              <label>Display Name: </label>
+              <input
+                type="text"
+                placeholder={user?.profilename}
+                value={userEdit.profilename}
+                onChange={(e) =>
+                  setUser({ ...userEdit, profilename: e.target.value })
+                }
+              />
+              <label>Username: </label>
+              <input
+                type="text"
+                placeholder={user?.username}
+                value={userEdit.username}
+                onChange={(e) =>
+                  setUser({ ...userEdit, username: e.target.value })
+                }
+              />
 
-                <button type="submit">Update Profile Information</button>
-              </form>
-            </div>
+              <button type="submit">Update Profile Information</button>
+            </form>
           ) : (
             <div className="bet-history-content">
               <h2>Bet History</h2>
