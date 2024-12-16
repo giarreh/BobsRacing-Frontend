0bac70b8a97b5c2c156c51978625df16213a4cee
Miadog7Extra <petter.karstensen@gmail.com>
2024-12-16 15:30:09 +0100

    merge changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import AppContextProvider from './contexts/AppContext';
-import Home from './pages/Home';
-import Error from './pages/Error';
-import SignIn from './pages/signIn/SignIn';
-import SignUp from './pages/signUp/SignUp';
-import Athletes from './pages/Athletes/Athletes';
-import AthleteDetails from './pages/Athletes/AthleteDetails';
-import UserContextProvider from './contexts/UserContext';
-import PrivateRoutes from './components/PrivateRoutes';
-import ProfilePage from './pages/profile/ProfilePage';
-import Races from './pages/races/Races';
-import CreateRace from './pages/races/create/CreateRace';
-import RaceDetails from './pages/races/details/RaceDetails';
-import Bets from './pages/bets/BetPage';
-=======
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
@@ -33,7 +14,8 @@ import Races from "./pages/races/Races";
 import CreateRace from "./pages/races/create/CreateRace";
 import RaceDetails from "./pages/races/details/RaceDetails";
 import { SignalRProvider } from "./contexts/SignalR/SignalRContext";
->>>>>>> main
+import Bets from './pages/bets/BetPage';
+
 
 const App: React.FC = () => {
   return (
