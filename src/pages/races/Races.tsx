ce930f6ba99a205ecee4474dd77ad1d0b3dd7565
stp <steventphung6@gmail.com>
2024-12-17 15:23:59 +0100

    Update Races.tsx

--- src/pages/races/Races.tsx
+++ src/pages/races/Races.tsx
@@ -3,37 +3,37 @@ import { useNavigate } from "react-router";
 import RaceItem from "./Item/RaceItem";
 import { AppContext } from "../../contexts/AppContext";
 import { Race } from "../../interfaces/IRace";
-
+ 
 export default function Races() {
   const navigate = useNavigate();
   const { races, athletes } = useContext(AppContext);
   const [isLoading, setIsLoading] = useState(true);
   const [unfinishedRaces, setUnfinishedRaces] = useState<Race[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
-
+ 
   useEffect(() => {
     // Check if races and athletes are loaded
     if (races && athletes) {
       // Safely filter unfinished races
-      const filteredRaces = races.filter((race) =>
-        race.raceId.toString().includes(searchQuery)
-      );
-      
-
+      const filteredRaces = races.filter((race) => !race.isFinished);
+ 
       // show earliest race at top
       const sortedRaces = filteredRaces.sort(
         (a, b) => new Date(a.date) - new Date(b.date)
         );
-
+ 
       setUnfinishedRaces(sortedRaces);
       setIsLoading(false);
     }
   }, [races, athletes]);
 
+  const filteredRaces = unfinishedRaces.filter((race) =>
+    race.raceId.toString().includes(searchQuery) // Compare raceId with searchQuery
+  );
   if (isLoading) {
     return <p>Loading...</p>; // You can show a loading spinner or message
   }
-  
+ 
   return (
     <div>
       <h1 onClick={() => navigate("/createrace")}>Create a race!</h1>
@@ -49,8 +49,8 @@ export default function Races() {
       </div>
 
       <div>
-        {unfinishedRaces.length > 0 ? (
-          unfinishedRaces.map((race: Race, index) => (
+        {filteredRaces.length > 0 ? (
+          filteredRaces.map((race: Race, index) => (
             <RaceItem
               key={race.raceId} // Use a unique ID for keys
               race={race}
