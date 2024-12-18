bbcfe20667bcd7ca9fdd010a59267b91a4e7438d
giarreh <giarreh1@gmail.com>
2024-12-18 10:41:07 +0100

    adding restrict mode back to prepare for production

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from 'react'
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
